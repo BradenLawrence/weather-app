b9c31988804cb5bc5c13303027de60285dc18769
blip <b@bradenlawrence.com>
2017-12-10 23:08:45 -0500

    Update search bar to have value controlled by component state

--- src/containers/searchBar.js
+++ src/containers/searchBar.js
@@ -1,10 +1,26 @@
 import React, { Component }     from 'react'
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            term: ''
+        }
+    }
+
+    onInputChange = (event) => {
+        this.setState({ term: event.target.value })
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input/>
+                <input 
+                    className = "form-control" 
+                    placeholder = "Search for weather reports by city..."
+                    value = { this.state.term }
+                    onChange = { this.onInputChange }
+                />
                 <span className="input-group-btn">
                     <button className="btn btn-secondary" type="submit">Submit</button>
                 </span>
