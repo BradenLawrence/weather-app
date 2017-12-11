c1e7082e6842778754e228a6852dae9761ebec8b
blip <b@bradenlawrence.com>
2017-12-10 21:53:39 -0500

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
+import React                            from 'react';
+import ReactDOM                         from 'react-dom';
+import { Provider }                     from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import App                              from './components/app';
+import reducers                         from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
