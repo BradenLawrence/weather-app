eb018359ddf043a0c288ef368d2c8eebd1fa9bb4
blip <b@bradenlawrence.com>
2017-12-13 19:48:50 -0500

    Update URL with correct formatting for openweathermap.org

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,14 @@
 import axios from 'axios'
 
 const API_KEY =     '392245380b7f86f2b099e5dd69b122a8'
-const BASE_URL =    `api.openweathermap.org/data/2.5/weather?APPID=${API_KEY}&q=`
-// api.openweathermap.org/data/2.5/weather?q={city name}
+const BASE_URL =    'http://api.openweathermap.org/data/2.5/weather?q='
+// api.openweathermap.org/data/2.5/weather?q={city name},{country code}
 
 const GET_WEATHER = 'GET_WEATHER'
 
 const getWeather = function(city) {
-    const url =     BASE_URL + city
+    const url =     `${BASE_URL}${city},us&APPID=${API_KEY}`
+    
     const request = axios.get(url)
     return {
         type:       GET_WEATHER,
