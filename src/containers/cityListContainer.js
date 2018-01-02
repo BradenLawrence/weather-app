6866418b796bc8ad4a34911bf2b4aa75d376f742
blip <b@bradenlawrence.com>
2018-01-01 23:13:03 -0500

    Add Google Maps to city name

--- src/containers/cityListContainer.js
+++ src/containers/cityListContainer.js
@@ -1,16 +1,21 @@
 import React, { Component }     from 'react'
 import { connect }              from 'react-redux'
+import { Map }                  from '../components/map';
 import { Chart }                from '../components/chart';
 
 class CityList extends Component {
     renderWeather(cityData) {
         // Create arrays of our data
-        let temperatureList = cityData.list.map(listItem => listItem.main.temp * 9/5 - 459.67)
+        let temperatureList = cityData.list.map(listItem => listItem.main.temp * 9/5 - 459.67) // Conversion from kelvin to fahrenheit
         let pressureList    = cityData.list.map(listItem => listItem.main.pressure)
         let humidityList    = cityData.list.map(listItem => listItem.main.humidity)
+        let { lat, lon }    = cityData.city.coord
         return (
             <tr key={ cityData.city.id }>
-                <td>{ cityData.city.name }</td>
+                <td>
+                    <Map lon={ lon } lat={ lat }/>
+                    { cityData.city.name }
+                </td>
                 <td>
                     <Chart  
                         height  = {120} 
