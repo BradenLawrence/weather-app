bae00a7fcbd391b49b43b09d360bd84c64a2a77d
blip <b@bradenlawrence.com>
2018-01-17 23:31:56 -0500

    add basic error handling for invalid cities

--- src/reducers/weatherReducer.js
+++ src/reducers/weatherReducer.js
@@ -1,10 +1,15 @@
 import { GET_WEATHER } from "../actions/index";
 
 const weatherReducer = (state = [], action) => {
-    switch (action.type) {
-        // The ... spread operator takes all of the items currently in state and 'spread's them out into new items in this new array.
-        case GET_WEATHER: return [ action.payload.data, ...state ] 
-    }
+        switch (action.type) {
+            // The ... spread operator takes all of the items currently in state and 'spread's them out into new items in this new array.
+            case GET_WEATHER: {
+                if(action.error){
+                    return state
+                }
+                return [ action.payload.data, ...state ]
+            } 
+        }
     return state
 }
 
