431793091e00901b6f8101b0a3bf69f68980ce3c
blip <b@bradenlawrence.com>
2018-01-17 20:48:36 -0500

    change http:// to https://

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API_KEY =     '392245380b7f86f2b099e5dd69b122a8'
-const BASE_URL =    'http://api.openweathermap.org/data/2.5/forecast?q='
+const BASE_URL =    'https://api.openweathermap.org/data/2.5/forecast?q='
 // api.openweathermap.org/data/2.5/weather?q={city name},{country code}
 
 const GET_WEATHER = 'GET_WEATHER'
